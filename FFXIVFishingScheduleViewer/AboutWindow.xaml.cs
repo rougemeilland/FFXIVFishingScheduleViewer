fb11c7a207de3bce1954ef1c05a806bc5d52de1b
Lunor Kisasage <lunor.kisasage@mbr.nifty.com>
2020-07-22 19:48:21 +0900

    バージョン情報画面のタイトルの修正

--- FFXIVFishingScheduleViewer/AboutWindow.xaml.cs
+++ FFXIVFishingScheduleViewer/AboutWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 
 namespace FFXIVFishingScheduleViewer
 {
@@ -7,9 +9,36 @@ namespace FFXIVFishingScheduleViewer
     /// </summary>
     public partial class AboutWindow : Window
     {
+        private AboutViewModel _dataContext;
+
         public AboutWindow()
         {
             InitializeComponent();
+
+            _dataContext = null;
+            DataContext = _dataContext;
+
+            SetDataContext(DataContext);
+            if (_dataContext != null)
+            {
+                UpdateWindowTitle();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (_dataContext != null)
+                _dataContext.PropertyChanged -= _dataContext_PropertyChanged;
+            base.OnClosed(e);
+        }
+
+        private void Window_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            SetDataContext(e.NewValue);
+            if (_dataContext != null)
+            {
+                UpdateWindowTitle();
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -17,5 +46,36 @@ namespace FFXIVFishingScheduleViewer
             DialogResult = true;
             Close();
         }
+
+        private void _dataContext_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(_dataContext.AboutWindowTitleText):
+                    UpdateWindowTitle();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetDataContext(object o)
+        {
+            if (_dataContext != null)
+                _dataContext.PropertyChanged -= _dataContext_PropertyChanged;
+            if (o != null && o is AboutViewModel)
+            {
+                _dataContext = (AboutViewModel)o;
+                _dataContext.PropertyChanged += _dataContext_PropertyChanged;
+            }
+            else
+                _dataContext = null;
+        }
+
+        private void UpdateWindowTitle()
+        {
+            Title = string.Format("{0} - {1}", _dataContext.AboutWindowTitleText, Owner.Title);
+        }
+
     }
 }
